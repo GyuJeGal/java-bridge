a0393e53cc6ad0db7a7e856b8c765245a86bd3ce
GyuJeGal <lkjdee0329@gmail.com>
2022-11-20 18:41:07 +0900

    feat(InputView): 세팅할 다리의 길이를 입력받는 기능 구현 다리의 길이를 입력받고 유효성 체크를 거친 후 정수 타입으로 반환

--- src/main/java/bridge/ui/InputView.java
+++ src/main/java/bridge/ui/InputView.java
@@ -1,15 +1,29 @@
-package bridge;
+package bridge.ui;
+
+import bridge.exception.InputException;
+import bridge.utils.InputUtil;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private final InputException inputException;
+    private final InputUtil inputUtil;
+
+    public InputView() {
+        this.inputException = new InputException();
+        this.inputUtil = new InputUtil();
+    }
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine();
+        inputException.checkBridgeSize(input);
+        return inputUtil.convertToInt(input);
     }
 
     /**
